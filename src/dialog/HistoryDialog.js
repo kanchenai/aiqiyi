12efaaba5b96191b57a21ad3e4cc53a457007979
mahangbin <2695704942@qq.com>
2023-03-03 15:37:40 +0800

    完善历史记录弹窗，在首页的右上角进入

--- src/dialog/HistoryDialog.js
+++ src/dialog/HistoryDialog.js
@@ -1,7 +1,10 @@
 import LoneDialog from "@core/frame/view/group/LoneDialog";
 import html from "@html/dialog/history_dialog.html"
+import {historyData} from "../mock-data/home/history_dialog_data";
+import VMargin from "../../core/frame/util/VMargin";
+import {Adapter} from "../../core/frame/view/group/RecycleView";
 
-export default class HistoryDialog extends LoneDialog{
+export default class HistoryDialog extends LoneDialog {
     constructor(viewManager) {
         super(viewManager);
 
@@ -11,9 +14,38 @@ export default class HistoryDialog extends LoneDialog{
         this.initUtils();
     }
 
-    initView(){
+    initView() {
+        this.content_list = this.findViewById("content_list");
+        console.log(this.content_list);
+        this.content_list.col = 3;
+        this.content_list.margin = new VMargin(0, 20, 0, 10);
+        this.content_list.adapter = new HistoryAdapter();
+    }
+
+    setView() {
+    }
+
+    initUtils() {
+    }
+
+    onVisibleChangeListener(view, isShowing) {
+        //弹窗显示并且未获取过数据的时候设置数据
+        if (isShowing && (!this.content_list.data || this.content_list.data.length == 0)) {
+            this.content_list.data = historyData;
+        }
+    }
+}
+
+class HistoryAdapter extends Adapter {
+    bindHolder(holder, data) {
+        var pic = holder.findViewById("pic");
+        var name = holder.findViewById("name");
+        var info1 = holder.findEleById("info1");
+        var info2 = holder.findEleById("info2");
+        pic.src = data.pic;
+        name.text = data.name;
+        info1.innerText = data.info1;
+        info2.innerText = data.info2;
 
     }
-    setView(){}
-    initUtils(){}
 }
---
+++ src/mock-data/home/history_dialog_data.js
@@ -0,0 +1,59 @@
+import hot_01 from "@images-js/home/recommend_fragment/hot_01.png"
+import hot_02 from "@images-js/home/recommend_fragment/hot_02.png"
+import hot_03 from "@images-js/home/recommend_fragment/hot_03.png"
+import hot_04 from "@images-js/home/recommend_fragment/hot_04.png"
+import hot_05 from "@images-js/home/recommend_fragment/hot_05.png"
+import hot_06 from "@images-js/home/recommend_fragment/hot_06.png"
+import hot_07 from "@images-js/home/recommend_fragment/hot_07.png"
+import hot_08 from "@images-js/home/recommend_fragment/hot_08.png"
+
+export var historyData = [
+    {
+        name: "这个杀手不太冷静",
+        info1: "02:04:03",
+        info2: "观看至66%",
+        pic: hot_01
+    },
+    {
+        name: "独行月球",
+        info1: "01:54:03",
+        info2: "观看至33%",
+        pic: hot_02
+    },
+    {
+        name: "一盘大棋",
+        info1: "01:44:03",
+        info2: "观看至34%",
+        pic: hot_03
+    },
+    {
+        name: "外太空的莫扎特",
+        info1: "01:54:13",
+        info2: "观看至13%",
+        pic: hot_04
+    },
+    {
+        name: "发财日记",
+        info1: "01:34:03",
+        info2: "观看至23%",
+        pic: hot_05
+    },
+    {
+        name: "唐人街探案3",
+        info1: "02:54:03",
+        info2: "观看至63%",
+        pic: hot_06
+    },
+    {
+        name: "一点就到家",
+        info1: "01:54:03",
+        info2: "观看至33%",
+        pic: hot_07
+    },
+    {
+        name: "狼群",
+        info1: "02:14:03",
+        info2: "观看至37%",
+        pic: hot_08
+    }
+]
