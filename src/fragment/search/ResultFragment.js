8a3af5410c87b367a10aeac2874cf4577b6e89cb
mahangbin <2695704942@qq.com>
2023-03-08 20:30:54 +0800

    完善搜索页，搜索页分推荐分页、搜索结果分页，输入搜索关键字，会切换到搜索结果分页，置空回到推荐分页

--- src/fragment/search/ResultFragment.js
+++ src/fragment/search/ResultFragment.js
@@ -1,6 +1,8 @@
 import Fragment from "../../../core/frame/view/group/Fragment";
 
 import html from "../../html/fragment/search/result_fragment.html"
+import {topicData_0} from "../../mock-data/home/recommend_fragement_data";
+import {Adapter} from "../../../core/frame/view/group/RecycleView";
 
 export default class ResultFragment extends Fragment{
     onCreate() {
@@ -11,7 +13,44 @@ export default class ResultFragment extends Fragment{
     }
 
     initView(){
+        this.result = this.findViewById("result");
+        this.result.adapter = new ResultAdapter();
+
+        this.recommend = this.findViewById("recommend");
+        this.recommend.adapter = new TextAdapter();
+
+        this.hot = this.findViewById("hot");
+        this.hot.adapter = new TextAdapter();
     }
     setView(){}
-    initUtil(){}
+    initUtil(){
+        this.result.data = topicData_0;
+
+        this.recommend.data = ["人世间", "辛亥革命", "龙珠超", "觉醒年代", "狙击之王", "集结号"];
+        this.hot.data = [
+            "世界杯", "一年一度喜剧大会", "卿卿日常", "唐朝记事录", "王牌对王牌",
+            "苍兰诀","我们这十年","奔跑吧","爱情公寓","底线"
+        ];
+    }
+}
+
+class ResultAdapter extends Adapter {
+    bindHolder(holder, data) {
+        var pic = holder.findViewById("pic");
+        var name = holder.findViewById("name");
+        var info1 = holder.findEleById("info1");
+        var info2 = holder.findEleById("info2");
+        pic.src = data.pic;
+        name.text = data.name;
+        info1.innerText = data.info1;
+        info2.innerText = data.info2;
+
+    }
+}
+
+class TextAdapter extends Adapter {
+    bindHolder(holder, data) {
+        var text = holder.findViewById("text");
+        text.text = data;
+    }
 }
