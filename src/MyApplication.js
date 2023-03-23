e26836639e12f089b9f68d227d53397105e3372a
mahangbin <2695704942@qq.com>
2023-03-23 17:25:04 +0800

    进入app时保存返回数据并做出处理，在返回时做出对应处理

--- src/MyApplication.js
+++ src/MyApplication.js
@@ -1,10 +1,11 @@
-import Application from "../core/frame/app/Application";
+import Application, {LaunchMode} from "../core/frame/app/Application";
 import HomePage from "@page/HomePage";
 import AliWebPlayer from "@src/util/AliWebPlayer";
 import WelcomePage from "@page/WelcomePage";
 import OrderPage from "./page/OrderPage";
 import SearchPage from "./page/SearchPage";
 import DetailPage from "./page/DetailPage";
+import ListPage from "@page/ListPage";
 
 require('./global_style.css')
 
@@ -29,6 +30,9 @@ export default class MyApplication extends Application {
                 case "DetailPage":
                     page = new DetailPage();
                     break;
+                case "ListPage":
+                    page = new ListPage();
+                    break;
             }
             return page;
         }
@@ -38,6 +42,8 @@ export default class MyApplication extends Application {
         console.log("onLaunch，地址栏参数：", urlParam);
         var firstPage = null;
         var param = null;//将地址栏参数中与firstPage相关的参数填到param，会在firstPage中获取到
+        this.backUrl = decodeURIComponent(urlParam.backUrl || "");
+
         switch (urlParam.pageKey) {
             case "welcome":
                 firstPage = new WelcomePage();
@@ -58,9 +64,22 @@ export default class MyApplication extends Application {
         return {firstPage, param};
     }
 
+    /**
+     * 这个方法先于启动firstPage
+     * @param page
+     * @param param
+     */
+    onCreate(page, param) {
+        let urlParam = this.urlParam;
+        if (this.launchMode == LaunchMode.ENTER && urlParam.pageKey && urlParam.pageKey != "home") {//进入专区，并且第一个页面不是首页
+            //TODO 这里还可以从参数中获取是否需要返回首页的标识，区分firstPage返回
+            var homePage = new HomePage();
+            this.startPage(homePage, null);
+        }
+    }
+
     exitUrl() {
-        var url = "";
-        return url;
+        return this.backUrl;
     }
 
     getPlayerInstance() {
